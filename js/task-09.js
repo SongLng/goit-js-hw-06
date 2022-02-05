94716fea5c16de63764aa93b5a3c389a87fdde9d
Sergio Pieshko <sergio.pieshko@icloud.com>
2022-02-05 16:37:42 +0200

    complete 1

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const hexText = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  hexText.textContent = getRandomHexColor();
+});
